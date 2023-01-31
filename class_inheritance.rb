b314794a02f06f3e5b7ad1553081d73e15a33724
Yong <yong.lin13@gmail>
2023-01-31 15:36:28 -0500

    Worked on Bonus

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -24,13 +24,25 @@ class Manager < Employee
     end
 
     def bonus(multiplier)
-        return @employees[0].salary * multiplier if @employees.length == 1
+        #Need a new base case, 
+        #return self.bonus(multiplier) if self.employees.nil?
         sum = 0
-        @employees.each do |employee|
-            return sum * multiplier if employee.nil?
-            sum += employee.bonus(multiplier)
+        queue = [self]
+        while !queue.empty?
+            front_q = queue.shift
+            if front_q.is_a?(Manager)
+                front_q.employees.each do |employee|
+                    queue << employee 
+                    sum += employee.salary
+                end
+            end
+        # @employees.each do |employee|
+        #     new_bonus = employee.bonus(multiplier)
+        #     sum += new_bonus
+        #     # return sum * multiplier if employee.nil?
+        #     # sum += employee.bonus(multiplier)
         end
-        sum
+        sum * multiplier
     end
  
 end
